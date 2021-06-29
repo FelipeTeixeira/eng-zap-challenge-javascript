2a72c418cfa53a02705ffe0739b9a6794bb28c5f
Felipe Teixeira <felipe.eteixeira@mercadolivre.com>
2021-06-29 20:33:48 -0300

    refactoring SCSS

--- src/app/shared/services/property.resolver.ts
+++ src/app/shared/services/property.resolver.ts
@@ -6,45 +6,75 @@ import { isPlatformServer } from '@angular/common';
 import { makeStateKey, TransferState } from '@angular/platform-browser';
 import { PropertyService } from './property.service';
 import { Property } from '../model/property';
+import { RegrasUtils } from '../utils/regras.util';
 
 
 @Injectable()
-export class PropertyResolver implements Resolve<Property> {
+export class PropertyResolver implements Resolve<Property[]> {
 
     constructor(
         private propertyService: PropertyService,
         private transferState: TransferState,
+        private regrasUtils: RegrasUtils,
         @Inject(PLATFORM_ID) private platformId
     ) { }
 
     resolve(
         route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot
-    ): Observable<Property> {
+    ): Observable<Property[]> {
 
-        const propertyId = route.params['id'];
-
-        const PROPERTY_KEY = makeStateKey<Property>('propertyKey-' + propertyId);
+        const PROPERTY_KEY = makeStateKey<Property[]>('propertyKey-olx-br');
 
         if (this.transferState.hasKey(PROPERTY_KEY)) {
-
-            const property = this.transferState.get(PROPERTY_KEY, null);
-
-            this.transferState.remove(PROPERTY_KEY);
-
-            return of(property);
+            return of(this.transferState.get(PROPERTY_KEY, null));
         }
         else {
-            // return this.propertyService.findCourseById(propertyId)
-            //     .pipe(
-            //         first(),
-            //         tap(property => {
-            //             if (isPlatformServer(this.platformId)) {
-            //                 this.transferState.set(PROPERTY_KEY, property);
-            //             }
-            //         })
-            //     );
+            const company = route.params['company'];
+
+            return this.propertyService.findAll()
+                .pipe(
+                    first(),
+                    tap(properties => {
+                        const filterProperty = (item: Property) => {
+                            const { pricingInfos, usableAreas, address } = item;
+                            const { price, businessType, monthlyCondoFee } = pricingInfos;
+                            const { lon, lat } = address.geoLocation.location;
+
+                            let isAvailableForRental = false;
+                            let isAvailableForSale = false;
+
+                            if (company === 'zap-imoveis') {
+                                // TO DO, ta errado
+                                isAvailableForRental = false;
+
+                                isAvailableForSale = this.regrasUtils.temLatLon(lon, lat)
+                                    && this.regrasUtils.isSale(businessType)
+                                    && this.regrasUtils.valorMinimoVenda(price)
+                                    && this.regrasUtils.valorMinimoMetroQuadrado(price, usableAreas);
+                            } else {
+                                isAvailableForRental = this.regrasUtils.temLatLon(lon, lat)
+                                    && this.regrasUtils.isRental(businessType)
+                                    && this.regrasUtils.valorMaximoAluguel(price, monthlyCondoFee)
+                                    && this.regrasUtils.valorLimitCondomio(price, monthlyCondoFee)
+
+                                isAvailableForSale = this.regrasUtils.temLatLon(lon, lat)
+                                    && this.regrasUtils.isSale(businessType)
+                                    && this.regrasUtils.valorMaximoVenda(price)
+                            }
+
+                            return isAvailableForRental || isAvailableForSale;
+                        }
+
+                        this.transferState.set(PROPERTY_KEY, properties.filter(filterProperty));
+                    })
+                );
         }
     }
-
 }
+
+//
+/*
+    bug de quando entra na pagina,
+    preciso salvar todos os dados de uma vez, quando entrar aqui gerar um array pro zap e um pro viva
+*/
