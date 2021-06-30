f7415e6696f56f0c2346922ece1fda92cf37cd68
Felipe Teixeira <felipe.eteixeira@mercadolivre.com>
2021-06-30 00:20:59 -0300

    update tag

--- src/app/shared/services/meta-tag-seo.service.ts
+++ src/app/shared/services/meta-tag-seo.service.ts
@@ -15,22 +15,22 @@ export class MetaTagSeoService {
     setMetaTags(title: string, description: string) {
         // SEO metadata
         this.title.setTitle(title);
-        this.meta.addTag({ name: 'description', title });
+        this.meta.updateTag({ name: 'description', description });
 
         // Facebook & Instagram cards
-        this.meta.addTag({ name: 'og:type', content: 'website' });
-        this.meta.addTag({ name: 'og:title', content: '@zapimoveis' });
-        this.meta.addTag({ name: 'og:image', content: 'ttps://assets.zap.com.br/assets/v5.115.0/zap-imoveis-ok.png' });
-        this.meta.addTag({ name: 'og:site_name', content: 'Grupo ZAP - Abrindo caminhos para o mercado imobiliário no Brasil' });
-        this.meta.addTag({ name: 'og:url', content: this.dom.URL });
-        this.meta.addTag({ name: 'og:description', content: description });
+        this.meta.updateTag({ name: 'og:type', content: 'website' });
+        this.meta.updateTag({ name: 'og:title', content: '@zapimoveis' });
+        this.meta.updateTag({ name: 'og:image', content: 'ttps://assets.zap.com.br/assets/v5.115.0/zap-imoveis-ok.png' });
+        this.meta.updateTag({ name: 'og:site_name', content: 'Grupo ZAP - Abrindo caminhos para o mercado imobiliário no Brasil' });
+        this.meta.updateTag({ name: 'og:url', content: this.dom.URL });
+        this.meta.updateTag({ name: 'og:description', content: description });
 
 
         // Twitter metadata
-        this.meta.addTag({ name: 'twitter:card', content: 'summary_large_image' });
-        this.meta.addTag({ name: 'twitter:site', content: '@zapimoveis' });
-        this.meta.addTag({ name: 'twitter:title', title });
-        this.meta.addTag({ name: 'twitter:description', description });
-        this.meta.addTag({ name: 'twitter:image', content: 'ttps://assets.zap.com.br/assets/v5.115.0/zap-imoveis-ok.png' });
+        this.meta.updateTag({ name: 'twitter:card', content: 'summary_large_image' });
+        this.meta.updateTag({ name: 'twitter:site', content: '@zapimoveis' });
+        this.meta.updateTag({ name: 'twitter:title', title });
+        this.meta.updateTag({ name: 'twitter:description', description });
+        this.meta.updateTag({ name: 'twitter:image', content: 'ttps://assets.zap.com.br/assets/v5.115.0/zap-imoveis-ok.png' });
     }
 }
