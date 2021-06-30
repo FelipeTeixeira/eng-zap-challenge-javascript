b3c6100e2b3c2fca7ac897e22db1c051823f2613
Felipe Teixeira <felipe.eteixeira@mercadolivre.com>
2021-06-29 23:57:41 -0300

    improvements SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CanonicalService } from './shared/services/canonical.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    constructor(
+        private canonicalService: CanonicalService
+    ) {}
+
+    ngOnInit() {
+        this.canonicalService.setCanonicalURL();
+    }
+ }
