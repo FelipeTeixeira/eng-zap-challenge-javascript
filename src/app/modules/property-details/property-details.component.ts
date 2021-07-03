26c1c557401cf1caa9fdfc26c45aacc895766709
Felipe Teixeira <felipe.eteixeira@mercadolivre.com>
2021-07-03 14:15:55 -0300

    details refactoring streaming

--- src/app/modules/property-details/property-details.component.ts
+++ src/app/modules/property-details/property-details.component.ts
@@ -1,7 +1,8 @@
 import { CurrencyPipe, DatePipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 import { Property } from 'src/app/shared/model/property';
 import { MetaTagSeoService } from 'src/app/shared/services/meta-tag-seo.service';
 import { PropertyService } from 'src/app/shared/services/property.service';
@@ -9,14 +10,15 @@ import { PropertyService } from 'src/app/shared/services/property.service';
 @Component({
     selector: 'app-property-details',
     templateUrl: './property-details.component.html',
-    styleUrls: ['./property-details.component.scss']
+    styleUrls: ['./property-details.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PropertyDetailsComponent implements OnInit {
     property$: Observable<Property>;
     propertiesSimilar$: Observable<Property[]>;
     propertyId: string;
     companySelected: string;
-    pagePagination: number;
+    currentPage: number;
 
     constructor(
         private propertyService: PropertyService,
@@ -27,20 +29,22 @@ export class PropertyDetailsComponent implements OnInit {
         private router: Router
     ) { }
 
-    ngOnInit(): void {
-        this.companySelected = this.activatedRoute.snapshot.params['company'];
-        this.propertyId = this.activatedRoute.snapshot.params['propertyId'];
+    ngOnInit() {
+        this.property$ = this.activatedRoute.params.pipe(
+            switchMap(params => {
+                this.propertyId = params['propertyId'];
+                this.companySelected = params['company'];
 
-        this.property$ = this.propertyService.findProperty(this.propertyId, this.companySelected);
-        this.propertiesSimilar$ = this.propertyService.findPropertiesSimilar(this.propertyId, this.companySelected);
+                return this.propertyService.findProperty(params['propertyId'], params['company']);
+            }),
+            tap((property) => {
+                this.propertiesSimilar$ = this.propertyService.findPropertiesSimilar(this.propertyId, this.companySelected);
+                this.setTags(property);
+            })
+        );
 
-        this.property$.subscribe(response => {
-            this.setTags(response);
-        });
-
-        if (this.activatedRoute.snapshot.queryParams.page) {
-            this.pagePagination = this.activatedRoute.snapshot.queryParams.page;
-        }
+        const queryParamsPage = this.activatedRoute.snapshot.queryParams.page;
+        this.currentPage = queryParamsPage ? queryParamsPage : 1;
     }
 
     setTags(property: Property) {
@@ -53,8 +57,9 @@ export class PropertyDetailsComponent implements OnInit {
     backProperties() {
         this.router.navigate(['/empresa/', this.companySelected], {
             queryParams: {
-                page: this.pagePagination
-            }
+                page: this.currentPage
+            },
+            fragment: this.propertyId
         });
     }
 }
