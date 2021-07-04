8d99a8e38f63ff2d0f8231167795523b75cb567d
Felipe Teixeira <felipe.eteixeira@mercadolivre.com>
2021-07-04 13:18:18 -0300

    config test

--- src/app/modules/properties-list/properties-list.component.spec.ts
+++ src/app/modules/properties-list/properties-list.component.spec.ts
@@ -1,9 +1,9 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { SharedModule } from 'src/app/shared/shared.module';
 
 import { PropertiesListComponent } from './properties-list.component';
+import { PropertiesListModule } from './properties-list.module';
 
 describe('PropertiesListComponent', () => {
     let component: PropertiesListComponent;
@@ -15,7 +15,7 @@ describe('PropertiesListComponent', () => {
             imports: [
                 RouterTestingModule,
                 HttpClientTestingModule,
-                SharedModule
+                PropertiesListModule
             ],
         }).compileComponents();
     });
